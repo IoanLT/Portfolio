dc357bbaa55ea2362a3fbab03ad7325904dbd877
IoanLT <ioan.tranole@gmail.com>
2021-04-02 18:18:07 +0100

    adding content and styles for the Authors project page

--- src/pages/Authors.js
+++ src/pages/Authors.js
@@ -1,11 +1,111 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { HashLink as Link } from 'react-router-hash-link';
+import authorsDevices from '../assets/authors-devices.png';
+// Animations
+import { motion } from 'framer-motion';
+import { pageAnimation, textFade, photoAnimation, lineAnimation, contentAnimation, wrapAnimation } from '../animation';
+
+import { MusicMain, Banner, Devices, Concept, Development, Back } from '../pages/MusicPlayer';
+import arrowRight from '../assets/Arrow-right-black.svg';
+import backArrow from '../assets/backArrow.svg';
+import line from '../assets/Line 54.svg';
 
 const Authors = () => {
-    return (
-        <div>
-            The Authors page
-        </div>
+
+    return (     
+        <AuthorsMain                           
+            variants={pageAnimation}
+            initial="hidden"
+            animate="show"
+            exit="exit"
+        >           
+            <Banner>
+                <motion.h2 variants={textFade}>
+                    Authxrs
+                </motion.h2>
+                <motion.div variants={lineAnimation} className="line"></motion.div>                
+                <Devices>
+                    <motion.img variants={photoAnimation} src={authorsDevices} alt="music player" />
+                </Devices>           
+            </Banner>
+
+            <Concept variants={wrapAnimation}>                
+                <motion.div 
+                    className="concept-heading"                                      
+                    variants={contentAnimation}
+                >
+                    <img src={line} alt="line" />
+                    <h3>Concept</h3>
+                </motion.div>
+                <motion.div 
+                    className="concept-text"                   
+                    variants={contentAnimation}
+                >
+                    <motion.p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.                        
+                    </motion.p>
+                </motion.div>
+                <motion.button variants={textFade}>
+                    <a
+                        href="https://the-authors.netlify.app/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >                    
+                        Visit Site
+                        <img src={arrowRight} alt="arrow-right" />                    
+                    </a>
+                </motion.button>
+            </Concept>
+
+            <Development variants={wrapAnimation}>                
+                <motion.div 
+                    className="development-heading"
+                    variants={contentAnimation}                    
+                >
+                    <img src={line} alt="line" />
+                    <h3>Development</h3>
+                </motion.div>
+                <motion.div 
+                    className="development-text"
+                    variants={contentAnimation}                    
+                >
+                    <motion.p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.                        
+                    </motion.p>
+                </motion.div>
+                <motion.button variants={textFade}>
+                    <a
+                        href="https://github.com/yuryiva/authors-frontend"
+                        target="_blank"
+                        rel="noreferrer"
+                    >                    
+                        See on Github
+                        <img src={arrowRight} alt="arrow-right" />                    
+                    </a>
+                </motion.button>
+            </Development>
+
+           <Link to="/#projects">            
+                <Back>                  
+                    <img src={backArrow} alt="arrow back" />
+                    <h4>BACK</h4>                    
+                </Back>           
+            </Link>
+        </AuthorsMain>        
     )
 }
+
+const AuthorsMain = styled(MusicMain)``
 
 export default Authors;
